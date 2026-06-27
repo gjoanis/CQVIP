7d5444d2e88c5ea3db35e0013f41621c02c0e2a6
gjoanis <134109090+gjoanis@users.noreply.github.com>
2026-06-27 01:34:14 -0700

    Initial CQVIP working prototype

---
+++ web/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(title="CQVIP")
+
+templates = Jinja2Templates(directory="web/templates")
+
+app.mount("/static", StaticFiles(directory="web/static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
