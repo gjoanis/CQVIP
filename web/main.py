7791927953cdf96dfce61b340ba8d6576de75c10
gjoanis <134109090+gjoanis@users.noreply.github.com>
2026-06-27 01:52:54 -0700

    Initial CQVIP working prototype

--- web/main.py
+++ web/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from app.services.cqvip_engine import CQVIPEngine
+
 
 app = FastAPI(title="CQVIP")
 
@@ -11,8 +14,21 @@ app.mount("/static", StaticFiles(directory="web/static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
+def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html",
+        context={}
+    )
+
+
+@app.post("/generate-package")
+def generate_package():
+    engine = CQVIPEngine()
+    engine.run()
+
+    return FileResponse(
+        "exports/Validation_Package.zip",
+        media_type="application/zip",
+        filename="Validation_Package.zip"
     )
